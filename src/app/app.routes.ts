2e575186e85edc00482d02999b00ba310934b98d
Niko <nikolaguemmoud@hotmail.fr>
2025-05-28 16:17:19 +0200

    add hero_section_first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { PageHomeComponent } from './home/page-home/page-home.component';
 
 export const routes: Routes = [
+    {path:"",redirectTo:"home",pathMatch:'full'},
+    {path:"home",component:PageHomeComponent}
 ];
---
+++ src/app/home/page-home/page-home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeroSectionComponent } from '../hero-section/hero-section.component';
+
+@Component({
+  selector: 'app-page-home',
+  imports: [HeroSectionComponent],
+  templateUrl: './page-home.component.html',
+  styleUrl: './page-home.component.css',
+  animations:[
+    
+  ]
+})
+export class PageHomeComponent {
+
+}
